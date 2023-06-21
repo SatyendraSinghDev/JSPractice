daa6443b9d2e9fbef62b7c14e73ed47cdfb2397b
SatyendraSinghDev <satyendrasinghdev2@gmail.com>
2023-06-21 14:37:23 +0530

    Add chapters

---
+++ 2.Javascript Practice Exercises/3.writeFunction.js
@@ -0,0 +1,7 @@
+var variable = 2;
+document.write("Hello World");
+document.write(variable);
+document.write(4 + 2);
+document.write("<br>");
+document.write("Hello World.<br>");
+document.write("Hello World.<br>" + variable + "<br>");
---
+++ 2.Javascript Practice Exercises/4.alertFunction.js
@@ -0,0 +1,6 @@
+var variable = 2;
+window.alert("Hello World");
+window.alert(variable);
+window.alert(4 + 2);
+window.alert("Hello World" + variable);
+window.alert("Hello World" + "</br>");
